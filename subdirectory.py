8d6638987f19c4711011766be79ab4be926d838a
Maserati <maserati@localhost.localdomain>
2025-04-24 20:27:42 -0400

    atualizacao

--- subdirectory.py
+++ subdirectory.py
@@ -1,10 +1,12 @@
-import requests
 from datetime import datetime as dt
 import time
 
 import functions
 
 def subdirectory(url, file, filter_status_code=[], timeout=10, SSL=True, proxies=None, interval=0, advanced=False):
+
+    # colocar um intervalo randomico
+    # colocar um gerador de user-agent randomico
 
     payloads = functions.fileReader(file)
     if not payloads:
@@ -15,16 +17,13 @@ def subdirectory(url, file, filter_status_code=[], timeout=10, SSL=True, proxies
         payload = payload.strip()
         test_url = f'{url}/{payload}'.replace('//', '/')
         test_url = 'https://' + test_url
-        try:
-            time.sleep(interval)
-            # request
-            r = requests.get(
-                test_url, 
-                timeout=timeout,
-                verify= SSL,
-                proxies=proxies,
-            )
-            time_now = dt.now().strftime('%d/%m/%Y %H:%M:%S')
+        
+        time.sleep(interval) # intervalo entre requisicoes
+        time_now = dt.now().strftime('%d/%m/%Y %H:%M:%S') # hora atual
+
+        status, r = functions.request(test_url, timeout=timeout, SSL=SSL, proxies=proxies) # requisicao
+        
+        if status:
             if len(filter_status_code) == 0 or (len(filter_status_code) > 0 and r.status_code in filter_status_code):
                 print(f'[+][{time_now}][{r.status_code}] {r.url}')
                 if advanced:
@@ -37,15 +36,8 @@ def subdirectory(url, file, filter_status_code=[], timeout=10, SSL=True, proxies
                         'cache-control': headers_lowercase.get('cache-control', 'Unknown'),
                     }
                     print(f'[+]{" "*3}{metadata}')
-        
-        except requests.exceptions.Timeout as timeout_error:
-            print(f'[-][{time_now}] Tempo limite de requisicao atingido: {r.url}')
-            continue
-        
-        except requests.RequestException as e:
-            print(f'[-][{time_now}] Erro ao tentar o payload: {url} {payload} - {e}')
-            continue
-
+        else:
+            print(f'[-][{time_now}] {r}')
 
 
 # Example use
