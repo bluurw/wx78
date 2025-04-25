390b5c4e9363633a890eff7879f416be5faacea0
Maserati <maserati@localhost.localdomain>
2025-04-24 21:03:24 -0400

    atualizacao

--- subdirectory.py
+++ subdirectory.py
@@ -3,10 +3,12 @@ import time
 
 import functions
 
-def subdirectory(url, file, filter_status_code=[], timeout=10, SSL=True, proxies=None, interval=0, advanced=False):
+def subdirectory(url, file, filter_status_code=[], timeout=10, SSL=True, redirect=False, proxies=None, interval=0):
 
     # colocar um intervalo randomico
     # colocar um gerador de user-agent randomico
+    # alterar o test_url para que aceite http tambem
+    # verificar como retornar dados do ssl
 
     payloads = functions.fileReader(file)
     if not payloads:
@@ -20,30 +22,20 @@ def subdirectory(url, file, filter_status_code=[], timeout=10, SSL=True, proxies
         
         time.sleep(interval) # intervalo entre requisicoes
         time_now = dt.now().strftime('%d/%m/%Y %H:%M:%S') # hora atual
-
-        status, r = functions.request(test_url, timeout=timeout, SSL=SSL, proxies=proxies) # requisicao
+        # requisicao
+        status, r = functions.request(test_url, timeout=timeout, SSL=SSL, redirect=redirect, proxies=proxies) 
         
         if status:
             if len(filter_status_code) == 0 or (len(filter_status_code) > 0 and r.status_code in filter_status_code):
                 print(f'[+][{time_now}][{r.status_code}] {r.url}')
-                if advanced:
-                    headers_lowercase = {key.lower(): value.lower() for key, value in r.headers.items()}
-                    metadata = {
-                        'url': r.url,
-                        'server': headers_lowercase.get('server', 'Unknown'),
-                        'content-encoding': headers_lowercase.get('content-encoding', 'Unknown'),
-                        'content-type': headers_lowercase.get('content-type', 'Unknown'),
-                        'cache-control': headers_lowercase.get('cache-control', 'Unknown'),
-                    }
-                    print(f'[+]{" "*3}{metadata}')
         else:
             print(f'[-][{time_now}] {r}')
 
 
 # Example use
 subdirectory('pluxee.com.br',
-            '/home/maserati/Downloads/Python/wx78/wordlists/subdirectory/wordlist.txt',
+            'wordlists/subdirectory/wordlist.txt',
             timeout=10,
+            redirect=True,
             proxies=None, 
-            advanced=True)
-    
+            )
