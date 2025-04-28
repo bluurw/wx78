1ce2ce4b5e9c5a9bc01d97b33c9bcb2ad8a793d2
Maserati <maserati@localhost.localdomain>
2025-04-27 20:43:58 -0400

    trasnplantado o metaheaders como funcao

--- functions.py
+++ functions.py
@@ -32,5 +32,33 @@ def request(url, timeout, SSL, proxies=None, headers=None, ua_status=False, redi
             return False, f'Erro de certificado: {url} - {e}'
         try_requests -= 1
 
+
+def get_headers_metadata(response_headers):
+    if 'CaseInsensitiveDict' in str(type(response_headers)):
+        headers_lowercase = {key.lower(): value.lower() for key, value in response_headers.items()}
+        headers_metadata = {
+            'access-control-allow-credentials': headers_lowercase.get('access-control-allow-credentials', 'Unknown'), # indica login
+            'access-control-allow-headers': headers_lowercase.get('access-control-allow-headers', 'Unknown'),
+            'access-control-allow-methods': headers_lowercase.get('access-control-allow-methods', 'Unknown'),
+            'access-control-allow-origin': headers_lowercase.get('access-control-allow-origin', 'Unknown'),
+            'cache-control': headers_lowercase.get('cache-control', 'Unknown'),
+            'content-encoding': headers_lowercase.get('content-encoding', 'Unknown'),
+            'content-type': headers_lowercase.get('content-type', 'Unknown'),
+            'etag': headers_lowercase.get('etag', 'Unknown'),
+            'content-length': headers_lowercase.get('content-length', 'Unknown'),
+            'server': headers_lowercase.get('server', 'Unknown'), # servidor
+            'location': headers_lowercase.get('location', 'Unknown'),
+            'x-frame-options': headers_lowercase.get('x-frame-options', 'Unknown'), # previne clickjacking, se em falta o site e vulneravel
+            'x-matched-path': headers_lowercase.get('x-matched-path', 'Unknown'),
+            'x-powered-by': headers_lowercase.get('x-powered-by', 'Unknown'),
+            'X-sc-rewrite': headers_lowercase.get('X-sc-rewrite', 'Unknown'),
+            'transfer-encoding': headers_lowercase.get('transfer-encoding', 'Unknown'),
+            'set-cookie': headers_lowercase.get('set-cookie', 'Unknown'),
+        }
+        return True, headers_metadata
+    else:
+        return False, 'dados invalidos'
+
+
 def time_now():
     return dt.now().strftime('%d/%m/%Y %H:%M:%S') # hora atual
