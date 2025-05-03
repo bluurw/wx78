de44e7a00b7722d7964633c3b7dfbd774ae4c238
Maserati <maserati@localhost.localdomain>
2025-05-03 12:21:56 -0400

    functions > utils

--- subdomain.py
+++ subdomain.py
@@ -1,8 +1,8 @@
 import asyncio
 
-import commons
-import functions
+import utils
 import jsonlog
+import commons
 import certificate
 
 async def subdomain(origin, file, filter_status_code=[], ua_status=False, timeout=10, SSL=True, redirect=False, proxies=None, interval=0, advanced=False):
@@ -45,7 +45,7 @@ async def subdomain(origin, file, filter_status_code=[], ua_status=False, timeou
                     redirect_history = [resp for resp in r.history if resp.status_code in [301, 302, 307]]    
                 
                 # ip sera coletado por padrao
-                status_ip, ip = functions.get_ip_host("".join(url[1:]))
+                status_ip, ip = utils.get_ip_host("".join(url[1:]))
             
             else:
                 if len(filter_status_code) == 0 or (len(filter_status_code) != 0 and 404 in filter_status_code):
