18f963c5a5b38284a62458f601b6318bdc5856b8
Maserati <maserati@localhost.localdomain>
2025-05-08 07:28:11 -0400

    add whois & dns

--- utils.py
+++ utils.py
@@ -2,23 +2,45 @@ import asyncio
 import socket
 import whois
 
+
 # host -> nome do host/domain str()
-def get_ip_host(host):
+def get_ip_host(hostname):
+    hostname_ = hostname.split('://')[1] if hostname.startswith('http://') or hostname.startswith('https://') else hostname
     try:
-        full_info = socket.gethostbyname_ex(host)
+        full_info = socket.gethostbyname_ex(hostname_)
         return True, full_info[2]
     except socket.gaierror:
         return False, 'Erro ao obter ip'
     except Exception as e:
         return False, f'Qualquer erro foi retornado {e}'
 
+
 # domain -> recebe host/domain str()
-def get_whois(domain):
+def get_whois(hostname):
+    hostname_ = hostname.split('://')[1] if hostname.startswith('http://') or hostname.startswith('https://') else hostname
     try:
-        inquiry = whois.whois(domain)
+        inquiry = whois.whois(hostname_)
         return True, inquiry
     except Exception as e:
         return False, f'Erro ao consultar informacoes do dominio {e}'
+
+
+# hostname -> recebe host/domain str()
+def get_dns(hostname):
+    dtdns = {}
+    hostname_ = hostname.split('://')[1] if hostname.startswith('http://') or hostname.startswith('https://') else hostname
+    record_types = ['A', 'AAAA', 'MX', 'NS', 'TXT', 'CNAME', 'SOA', 'PTR', 'SRV', 'CAA']
+    for type_ in record_types:
+        try:
+            response = dns.resolver.resolve(hostname_, type_)
+            dtdns[type_] = [r.to_text() for r in response]
+        except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+            dtdns[type_] = 'Sem resposta'
+        except dns.resolver.Timeout:
+            dtdns[type_] = 'Timeout'
+        except Exception as err:
+            dtdns[type_] = 'Sem informacao'
+    return dtdns
 
 # response_headers -> recebe o headers como entrada {}
 async def get_headers_metadata(response_headers):
