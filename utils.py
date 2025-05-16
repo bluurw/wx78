edd25a31f16920b54c220e67917a727dd2514078
Maserati <maserati@localhost.localdomain>
2025-05-16 18:22:44 -0400

    codigo rescrito e restruturado

--- utils.py
+++ utils.py
@@ -1,50 +1,27 @@
 import asyncio
-import socket
-import whois
+import aiofiles
 from urllib.parse import urljoin
+from datetime import datetime as dt
+
+import commons
 
 
-# host -> nome do host/domain str()
-def get_ip_host(hostname):
-    hostname_ = hostname.split('://')[1] if hostname.startswith('http://') or hostname.startswith('https://') else hostname
+def time_now():
+    return dt.now().strftime('%d/%m/%Y %H:%M:%S') # hora atual
+
+
+# le wordlists
+# file -> caminho do arquivo
+async def fileReader(file):
     try:
-        full_info = socket.gethostbyname_ex(hostname_)
-        return True, full_info[2]
-    except socket.gaierror:
-        return False, 'Erro ao obter ip'
-    except Exception as e:
-        return False, f'Qualquer erro foi retornado {e}'
+        async with aiofiles.open(f'{file}', 'r') as f:
+            lines = await f.readlines()
+            return True, lines
+    except FileNotFoundError:
+        return False, 'Arquivo nao encontrado'
+    except Exception as err:
+        return False, err
 
-
-# domain -> recebe host/domain str()
-def get_whois(hostname):
-    hostname_ = hostname.split('://')[1] if hostname.startswith('http://') or hostname.startswith('https://') else hostname
-    try:
-        inquiry = whois.whois(hostname_)
-        return True, inquiry
-    except Exception as e:
-        return False, f'Erro ao consultar informacoes do dominio {e}'
-
-
-# hostname -> recebe host/domain str()
-def get_dns(hostname):
-    dtdns = {}
-    hostname_ = hostname.split('://')[1] if hostname.startswith('http://') or hostname.startswith('https://') else hostname
-    record_types = sorted([
-        'A', 'AAAA', 'ANY', 'CAA', 'CNAME', 'DNSKEY', 'DS',
-        'MX', 'NS', 'PTR', 'SOA', 'SRV', 'TLSA', 'TSIG', 'TXT',
-    ])
-    for type_ in record_types:
-        try:
-            response = dns.resolver.resolve(hostname_, type_)
-            dtdns[type_] = [r.to_text() for r in response]
-        except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
-            dtdns[type_] = 'Sem resposta'
-        except dns.resolver.Timeout:
-            dtdns[type_] = 'Timeout'
-        except Exception as err:
-            dtdns[type_] = 'Sem informacao'
-    return dtdns
 
 
 # url -> url original
@@ -53,29 +30,8 @@ def merge_url(url, outstreched):
     return urljoin(url, outstreched)
 
 
-# response_headers -> recebe o headers como entrada {}
-async def get_headers_metadata(response_headers):
-    if 'CaseInsensitiveDict' in str(type(response_headers)):
-        headers_lowercase = {key.lower(): value.lower() for key, value in response_headers.items()}
-        headers_metadata = {
-            'access-control-allow-credentials': headers_lowercase.get('access-control-allow-credentials', 'Unknown'), # indica login
-            'access-control-allow-headers': headers_lowercase.get('access-control-allow-headers', 'Unknown'),
-            'access-control-allow-methods': headers_lowercase.get('access-control-allow-methods', 'Unknown'),
-            'access-control-allow-origin': headers_lowercase.get('access-control-allow-origin', 'Unknown'),
-            'cache-control': headers_lowercase.get('cache-control', 'Unknown'),
-            'content-encoding': headers_lowercase.get('content-encoding', 'Unknown'),
-            'content-type': headers_lowercase.get('content-type', 'Unknown'),
-            'etag': headers_lowercase.get('etag', 'Unknown'),
-            'content-length': headers_lowercase.get('content-length', 'Unknown'),
-            'server': headers_lowercase.get('server', 'Unknown'), # servidor
-            'location': headers_lowercase.get('location', 'Unknown'),
-            'x-frame-options': headers_lowercase.get('x-frame-options', 'Unknown'), # previne clickjacking, se em falta o site e vulneravel
-            'x-matched-path': headers_lowercase.get('x-matched-path', 'Unknown'),
-            'x-powered-by': headers_lowercase.get('x-powered-by', 'Unknown'),
-            'X-sc-rewrite': headers_lowercase.get('X-sc-rewrite', 'Unknown'),
-            'transfer-encoding': headers_lowercase.get('transfer-encoding', 'Unknown'),
-            'set-cookie': headers_lowercase.get('set-cookie', 'Unknown'),
-        }
-        return True, headers_metadata
-    else:
-        return False, 'dados invalidos'
+
+def normalize_url(target, SSL):
+        if target.startswith(('http://', 'https://')):
+            return target
+        return f'https://{target}' if SSL else f'http://{target}'
