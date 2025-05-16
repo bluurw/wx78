5ec706df82525907b818ea47d245aba70ede6ad8
Maserati <maserati@localhost.localdomain>
2025-05-16 19:14:51 -0400

    Adicao dos modos de get_all_forms & get_details_forms

--- HTMLAnalitcs.py
+++ HTMLAnalitcs.py
@@ -1,8 +1,31 @@
 import re
+from bs4 import BeautifulSoup
+
+from Request import Request
+
+# target    -> alvo
+async def get_all_forms(target):
+    status, r = Request(target).request()
+    if status:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        return True, soup.find_all('form')
+    return False, 'Falha na requisição'
+
+
+# form      -> formularios encontrados
+# payload   -> carga
+async def get_form_details(form, payload):
+    action = form.get('action')
+    method = form.get('method', 'get').lower()
+    inputs = []
+    for input_tag in form.find_all('input'):
+        name = input_tag.get('name')
+        if name:
+            inputs.append({'type': input_tag.get('type', 'text'), 'name': name, 'value': payload})
+    return {'action': action, 'method': method, 'inputs': inputs}
+
 
 # response -> bruto text da requisicao
-
-
 # coleta telefones
 def get_all_telephones(response):
     set_ = set()
@@ -16,15 +39,6 @@ def get_all_telephones(response):
 def get_all_emails(response):
     set_ = set()
     rgx = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]')
-    for result in re.findall(rgx, response.text, re.DOTALL):
-        set_.add(result)
-    return set_
-
-
-# form
-def get_all_form(response):
-    set_ = set()
-    rgx = r'<form.*?>.*?</form>'
     for result in re.findall(rgx, response.text, re.DOTALL):
         set_.add(result)
     return set_
@@ -66,6 +80,17 @@ def get_all_path(response):
         urls.add(href[0])
     return urls
 
+
+# DESCONTINUADO
+# response  -> resposta bruta
+'''
+def get_all_form(response):
+    set_ = set()
+    rgx = r'<form.*?>.*?</form>'
+    for result in re.findall(rgx, response.text, re.DOTALL):
+        set_.add(result)
+    return set_
+'''
 
 # response.headers['server']
 # response.headers['content-type']
